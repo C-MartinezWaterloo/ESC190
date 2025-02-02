1f59245f6bd18de4b63f0d36462e86cc7c0bdb28
Charlie Martinez <109118451+C-MartinezWaterloo@users.noreply.github.com>
2025-02-02 00:30:22 -0500

    Update onlinelab1.c

--- onlinelab1.c
+++ onlinelab1.c
@@ -5,7 +5,9 @@
 
 int linear_search(int *a, int sz, int elem){
 
-    for(int i=0;i<sz;i++){
+    for(int i=0;i<sz; i++){
+
+        
         if(a[i] == elem){
             return i;
         }
@@ -13,6 +15,8 @@ int linear_search(int *a, int sz, int elem){
 
 
     }
+
+    // Returning -1 basically in case it doesn't go though 
 
     return -1;
 
@@ -23,15 +27,22 @@ int linear_search(int *a, int sz, int elem){
 
 
 void reverse_arr(int *arr, int sz) {
-    for (int i = 0; i < sz / 2; i++) {
+
+
+
+    
+    for (int i = 0; i < sz/2; i++) {
+        
         int middle = arr[i];
-        arr[i] = arr[sz - i - 1];
-        arr[sz - i - 1] = middle;
+        arr[i] = arr[sz- i - 1];
+
+        
+        arr[sz - i-1] = middle;
     }
 }
 
 int main() {
-    // Q0: Hello World
+
     printf("Hello World\n");
 
    
